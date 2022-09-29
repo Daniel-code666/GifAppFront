a1686c66fb24d7a1caf34b96fa9da2c4edfd76f7
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2022-09-29 16:44:55 -0500

    post gif

--- src/app/components/gif-add/gif-add.component.ts
+++ src/app/components/gif-add/gif-add.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GifServiceService } from 'src/app/services/gif-service.service';
-import { FormGroup, FormControl,FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Gif } from 'src/app/models/gif.model';
 
 @Component({
   selector: 'app-gif-add',
@@ -11,6 +12,7 @@ import { FormGroup, FormControl,FormBuilder } from '@angular/forms';
 export class GifAddComponent implements OnInit {
 
   form!: FormGroup;
+  selectedFile = null;
 
   constructor(private gifServ: GifServiceService, private router: Router) { }
 
@@ -22,7 +24,19 @@ export class GifAddComponent implements OnInit {
     gifRoute: new FormControl(),
   });
 
-  onSubmit(){
+  onSubmit() {
+    const gif = new Gif()
 
+    gif.gifTag = this.elemento.value.gifTag
+    gif.gifRoute = this.elemento.value.gifRoute
+
+    console.log(gif)
+
+    this.gifServ.postGif(gif).subscribe(success => {
+      console.log(success)
+      this.elemento.value.gifTag = ''
+      this.elemento.value.gifRoute = ''
+      this.router.navigate(['home'])
+    })
   }
 }
